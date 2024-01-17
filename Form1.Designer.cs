636ca70177ce82cc0fa25fe2e254f1821883849f
arantir <arantir40@gmail.com>
2024-01-17 22:19:54 +0500

    13

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            buttonTestsStart = new Button();
+            labelTest1 = new Label();
+            labelTest2 = new Label();
+            labelTest3 = new Label();
+            labelTest1Result = new Label();
+            labelTest2Result = new Label();
+            labelTest3Result = new Label();
+            labelStatus = new Label();
+            SuspendLayout();
             // 
-            // button1
+            // buttonTestsStart
             // 
-            this.button1.Location = new System.Drawing.Point(151, 109);
-            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "тест1";
-            this.button1.UseVisualStyleBackColor = true;
+            buttonTestsStart.Location = new Point(33, 402);
+            buttonTestsStart.Margin = new Padding(5);
+            buttonTestsStart.Name = "buttonTestsStart";
+            buttonTestsStart.Size = new Size(118, 38);
+            buttonTestsStart.TabIndex = 0;
+            buttonTestsStart.Text = "запуск";
+            buttonTestsStart.UseVisualStyleBackColor = true;
+            buttonTestsStart.Click += buttonTestsStart_Click;
+            // 
+            // labelTest1
+            // 
+            labelTest1.AutoSize = true;
+            labelTest1.Location = new Point(33, 45);
+            labelTest1.Name = "labelTest1";
+            labelTest1.Size = new Size(64, 25);
+            labelTest1.TabIndex = 1;
+            labelTest1.Text = "Тест 1";
+            // 
+            // labelTest2
+            // 
+            labelTest2.AutoSize = true;
+            labelTest2.Location = new Point(33, 96);
+            labelTest2.Name = "labelTest2";
+            labelTest2.Size = new Size(64, 25);
+            labelTest2.TabIndex = 2;
+            labelTest2.Text = "Тест 2";
+            // 
+            // labelTest3
+            // 
+            labelTest3.AutoSize = true;
+            labelTest3.Location = new Point(33, 148);
+            labelTest3.Name = "labelTest3";
+            labelTest3.Size = new Size(64, 25);
+            labelTest3.TabIndex = 3;
+            labelTest3.Text = "Тест 3";
+            // 
+            // labelTest1Result
+            // 
+            labelTest1Result.AutoSize = true;
+            labelTest1Result.Location = new Point(120, 45);
+            labelTest1Result.Name = "labelTest1Result";
+            labelTest1Result.Size = new Size(36, 25);
+            labelTest1Result.TabIndex = 4;
+            labelTest1Result.Text = "---";
+            // 
+            // labelTest2Result
+            // 
+            labelTest2Result.AutoSize = true;
+            labelTest2Result.Location = new Point(120, 96);
+            labelTest2Result.Name = "labelTest2Result";
+            labelTest2Result.Size = new Size(36, 25);
+            labelTest2Result.TabIndex = 5;
+            labelTest2Result.Text = "---";
+            // 
+            // labelTest3Result
+            // 
+            labelTest3Result.AutoSize = true;
+            labelTest3Result.Location = new Point(120, 148);
+            labelTest3Result.Name = "labelTest3Result";
+            labelTest3Result.Size = new Size(36, 25);
+            labelTest3Result.TabIndex = 6;
+            labelTest3Result.Text = "---";
+            // 
+            // labelStatus
+            // 
+            labelStatus.AutoSize = true;
+            labelStatus.Location = new Point(33, 283);
+            labelStatus.Name = "labelStatus";
+            labelStatus.Size = new Size(36, 25);
+            labelStatus.TabIndex = 7;
+            labelStatus.Text = "---";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 499);
-            this.Controls.Add(this.button1);
-            this.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(761, 499);
+            Controls.Add(labelStatus);
+            Controls.Add(labelTest3Result);
+            Controls.Add(labelTest2Result);
+            Controls.Add(labelTest1Result);
+            Controls.Add(labelTest3);
+            Controls.Add(labelTest2);
+            Controls.Add(labelTest1);
+            Controls.Add(buttonTestsStart);
+            Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
+        private Button buttonTestsStart;
+        private Label labelTest1;
+        private Label labelTest2;
+        private Label labelTest3;
+        private Label labelTest1Result;
+        private Label labelTest2Result;
+        private Label labelTest3Result;
+        private Label labelStatus;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,101 @@ namespace metranTest
 {
     public partial class Form1 : Form
     {
+        private Thread trd;
+        bool trdWork = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonTestsStart_Click(object sender, EventArgs e)
+        {
+            labelTest1Result.Text = labelTest2Result.Text = labelTest3Result.Text = "---";
+            buttonTestsStart.Enabled = false;
+            trdWork = true;
+        }
+
+        private void ThreadTask()
+        {
+            // Code that is executing when the thread is aborted.  
+            Random rnd = new Random();
+            int val;
+            while (true)
+            {
+                if (trdWork)
+                {
+
+                    labelStatus.Invoke(() => labelStatus.Text = "Выполнение теста 1");
+                    val = rnd.Next(1, 3);
+                    Thread.Sleep(val * 1000);
+                    val = rnd.Next(1, 3);
+                    if (val == 1)
+                    {
+                        labelTest1Result.Invoke(() => labelTest1Result.Text = "Успешно");
+                    }
+                    else
+                    {
+                        labelTest1Result.Invoke(() => labelTest1Result.Text = "Ошибка");
+                    }
+
+                    labelStatus.Invoke(() => labelStatus.Text = "Выполнение теста 2");
+                    val = rnd.Next(1, 3);
+                    Thread.Sleep(val * 1000);
+                    val = rnd.Next(1, 3);
+                    if (val == 1)
+                    {
+                        labelTest2Result.Invoke(() => labelTest2Result.Text = "Успешно");
+                    }
+                    else
+                    {
+                        labelTest2Result.Invoke(() => labelTest2Result.Text = "Ошибка");
+                    }
+
+                    labelStatus.Invoke(() => labelStatus.Text = "Выполнение теста 3");
+                    val = rnd.Next(1, 3);
+                    Thread.Sleep(val * 1000);
+                    val = rnd.Next(1, 3);
+                    if (val == 1)
+                    {
+                        labelTest3Result.Invoke(() => labelTest3Result.Text = "Успешно");
+                    }
+                    else
+                    {
+                        labelTest3Result.Invoke(() => labelTest3Result.Text = "Ошибка");
+                    }
+
+                    labelStatus.Invoke(() => labelStatus.Text = "---");
+                    buttonTestsStart.Invoke(() => buttonTestsStart.Enabled = true);
+                    trdWork = false;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trd = new Thread(new ThreadStart(this.ThreadTask));
+            trd.IsBackground = true;
+            trd.Start();
+        }
+        private void debugOut(string str)
+        {
+            MessageBox.Show(
+                str,
+                " ",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+        }
+        private void errorOut(string str)
+        {
+            MessageBox.Show(
+                str,
+                " ",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
         }
     }
 }
